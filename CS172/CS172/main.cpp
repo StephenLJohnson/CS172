328c9ac086921ef74408a5a5b550b7e7978151ec
Stephen Johnson <rearcog@gmail.com>
2016-09-12 16:30:56 -0700

    Initial commit

---
+++ CS172/CS172/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS172
+//
+//  Created by Stephen Johnson on 9/12/16.
+//  Copyright © 2016 Glacier Peak Studios LLC. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
